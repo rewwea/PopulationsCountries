c25e5fdd3af7bab4e0b36c51444547392bbb5844
rewwea <avandronov10@gmail.com>
2025-02-04 12:56:33 +0500

    Add functionality to filter countries by user-specified state and output populations

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,33 @@
 
 using namespace std;
 
-int main()
-{
-    ifstream fl("population.txt");
-    vector <string> countries;
-    string s;
-    double population = 0;
-    if (fl.is_open())
-    {
-        while (fl >> s)
-        {
-            if ((int(s[0]) > 57) and (int(s[0])> 47))
-            {
-                countries.push_back(s);
-            }
-            else
-            {
-                population += stoi(s);
-            }
+int main() {
+    fstream fl("population.txt");
+    vector<string> countries;
+    string s, state;
+    
+    cout << "Введите страну: ";
+    cin >> state;
+
+    while (getline(fl, s)) countries.push_back(s);
+    fl.close();
+
+    int place = 0;
+    double userPopulation = 0;
+    
+    for (auto v : countries) {
+        if (v.find(state) != string::npos) {
+            userPopulation = stod(v.substr(v.find_last_of(' ') + 1));
+            break;
         }
+        place++;
     }
-    cout << population << endl;
+
+    ofstream ur(state + ".txt");
+    for (int i = place + 1; i < countries.size(); i++) {
+        double pop = stod(countries[i].substr(countries[i].find_last_of(' ') + 1));
+        if (pop < userPopulation) ur << countries[i] << endl;
+    }
+
+    return 0;
 }
