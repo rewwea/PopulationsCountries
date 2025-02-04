58ea843f461d792451c525d7e691c70436127d3e
rewwea <avandronov10@gmail.com>
2025-02-04 13:04:25 +0500

    Add population data files for various countries

--- src/main.cpp
+++ src/main.cpp
@@ -5,33 +5,60 @@
 
 using namespace std;
 
-int main() {
-    fstream fl("population.txt");
-    vector<string> countries;
-    string s, state;
-    
-    cout << "Введите страну: ";
-    cin >> state;
+int main()
+{
+	ifstream f1("Population.txt");
+	vector<string> country;
+	string s;
 
-    while (getline(fl, s)) countries.push_back(s);
-    fl.close();
+	if (f1.is_open())
+	{
+		while (getline(f1, s))
+		{
+			country.push_back(s);
+		}
+	}
+	f1.close();
 
-    int place = 0;
-    double userPopulation = 0;
-    
-    for (auto v : countries) {
-        if (v.find(state) != string::npos) {
-            userPopulation = stod(v.substr(v.find_last_of(' ') + 1));
-            break;
-        }
-        place++;
-    }
+	for (int i = 65; i < 91; i++)
+	{
+		ofstream alphabet("Country/" + string(1, char(i)) + ".txt", ios::app);
+		for (auto v : country)
+		{
+			if (v[0] == char(i))
+			{
+				alphabet << v << endl;
+			}
+		}
+	}
 
-    ofstream ur(state + ".txt");
-    for (int i = place + 1; i < countries.size(); i++) {
-        double pop = stod(countries[i].substr(countries[i].find_last_of(' ') + 1));
-        if (pop < userPopulation) ur << countries[i] << endl;
-    }
+	for (int i = 65; i < 91; i++)
+	{
+		double total_population{ 0 };
+		int count = 0, big_countries = 0;
+		string strana;
+		ifstream alphabet_in("Country/" + string(1, char(i)) + ".txt");
 
-    return 0;
+		while (alphabet_in >> strana)
+		{
+			if (strana[0] >= '0' && strana[0] <= '9')
+			{
+				count++;
+				double population = stod(strana);
+				total_population += population;
+				if (population > 100000000)
+				{
+					big_countries++;
+				}
+			}
+		}
+		alphabet_in.close();
+
+		ofstream alphabet_out("Country/" + string(1, char(i)) + ".txt", ios::app);
+		alphabet_out << "\nОбщее население: " << total_population << endl;
+		alphabet_out << "Количество стран: " << count << endl;
+		alphabet_out << "Среднее население: " << (count ? total_population / count : 0) << endl;
+		alphabet_out << "Стран с населением более 100 млн: " << big_countries << endl;
+		alphabet_out.close();
+	}
 }
